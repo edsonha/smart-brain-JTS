fc841e06dc7ef0cacd60cac3d34c4d1227e323fe
edsonha <edsonha@gmail.com>
2020-06-17 17:12:41 +0800

    Implement session persistency and session sign in

--- src/App.js
+++ src/App.js
@@ -72,7 +72,20 @@ class App extends Component {
         .then((resp) => resp.json())
         .then((data) => {
           if (data && data.id) {
-            console.log("success we need to get user profile");
+            fetch(`http://localhost:3000/profile/${data.id}`, {
+              method: "get",
+              headers: {
+                "Content-Type": "application/json",
+                Authorization: token,
+              },
+            })
+              .then((resp) => resp.json())
+              .then((user) => {
+                if (user && user.email) {
+                  this.loadUser(user);
+                  this.onRouteChange("home");
+                }
+              });
           }
         })
         .catch((err) => console.log(err));
