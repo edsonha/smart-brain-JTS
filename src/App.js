6b6e27211a7a007a8419c6c7e2d3e39bbbf1c39c
edsonha <edsonha@gmail.com>
2020-06-17 18:04:58 +0800

    Set authorization header in the image frontend and rectify error of calculate face when unauthorized

--- src/App.js
+++ src/App.js
@@ -93,21 +93,26 @@ class App extends Component {
   }
 
   calculateFaceLocation = (data) => {
-    const clarifaiFaces = data.outputs[0].data.regions;
-    const image = document.getElementById("inputimage");
-    const width = Number(image.width);
-    const height = Number(image.height);
-    return clarifaiFaces.map((clarifaiFace) => ({
-      leftCol: clarifaiFace.region_info.bounding_box.left_col * width,
-      topRow: clarifaiFace.region_info.bounding_box.top_row * height,
-      rightCol: width - clarifaiFace.region_info.bounding_box.right_col * width,
-      bottomRow:
-        height - clarifaiFace.region_info.bounding_box.bottom_row * height,
-    }));
+    if (data && data.outputs) {
+      const clarifaiFaces = data.outputs[0].data.regions;
+      const image = document.getElementById("inputimage");
+      const width = Number(image.width);
+      const height = Number(image.height);
+      return clarifaiFaces.map((clarifaiFace) => ({
+        leftCol: clarifaiFace.region_info.bounding_box.left_col * width,
+        topRow: clarifaiFace.region_info.bounding_box.top_row * height,
+        rightCol:
+          width - clarifaiFace.region_info.bounding_box.right_col * width,
+        bottomRow:
+          height - clarifaiFace.region_info.bounding_box.bottom_row * height,
+      }));
+    }
   };
 
   displayFaceBox = (boxes) => {
-    this.setState({ boxes: boxes });
+    if (boxes) {
+      this.setState({ boxes: boxes });
+    }
   };
 
   onInputChange = (event) => {
@@ -118,7 +123,10 @@ class App extends Component {
     this.setState({ imageUrl: this.state.input });
     fetch("http://localhost:3000/imageurl", {
       method: "post",
-      headers: { "Content-Type": "application/json" },
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: window.sessionStorage.getItem("token"),
+      },
       body: JSON.stringify({
         input: this.state.input,
       }),
@@ -128,7 +136,10 @@ class App extends Component {
         if (response) {
           fetch("http://localhost:3000/image", {
             method: "put",
-            headers: { "Content-Type": "application/json" },
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: window.sessionStorage.getItem("token"),
+            },
             body: JSON.stringify({
               id: this.state.user.id,
             }),
